ff8cf65507c7a06f672522c4047c69ced22b7ae1
Erhan Yakut <yakuter@gmail.com>
2020-11-07 22:56:22 +0300

    added stringer

---
+++ 17-stringer/main.go
@@ -0,0 +1,47 @@
+// https://www.digitalocean.com/community/tutorials/how-to-use-interfaces-in-go
+
+package main
+
+import "fmt"
+
+type Article struct {
+	Title  string
+	Author string
+}
+
+func (a Article) String() string {
+	return fmt.Sprintf("The %q article was written by %s.", a.Title, a.Author)
+}
+
+type Book struct {
+	Title  string
+	Author string
+	Pages  int
+}
+
+func (b Book) String() string {
+	return fmt.Sprintf("The %q book was written by %s.", b.Title, b.Author)
+}
+
+type Stringer interface {
+	String() string
+}
+
+func Print(s Stringer) {
+	fmt.Println(s.String())
+}
+
+func main() {
+	a := Article{
+		Title:  "Understanding Interfaces in Go",
+		Author: "Sammy Shark",
+	}
+	Print(a)
+
+	b := Book{
+		Title:  "All About Go",
+		Author: "Jenny Dolphin",
+		Pages:  25,
+	}
+	Print(b)
+}
